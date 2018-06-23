ce7f535442190eebb6e84e0f1ecc6e41f042e9e7
storyteller <shashanksharma580S@gmail.com>
2018-06-23 14:20:27 +0530

    Service.json added

---
+++ app/src/main/java/com/example/shashank/myapplication/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shashank.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/example/shashank/myapplication/StartUp.kt
@@ -0,0 +1,12 @@
+package com.example.shashank.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up)
+    }
+}
